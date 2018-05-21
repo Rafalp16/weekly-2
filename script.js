3200f5ea92f6117a2dad2282aec2fd5fed951c66
Rafalp16 <rafalp16a@gmail.com>
2018-05-22 01:32:30 +0200

    fix menu, add cancel button on mobile

--- script.js
+++ script.js
@@ -67,22 +67,3 @@ function scrollSpy () {
 }
 
 document.addEventListener("scroll", scrollSpy);
-    
-// The function actually applying the offset
-function offsetAnchor() {
-  if (location.hash.length !== 0) {
-    window.scrollTo(window.scrollX, window.scrollY - 65);
-  }
-}
-
-// Captures click events of all <a> elements with href starting with #
-document.addEventListener('click', function(event) {
-  // Click events are captured before hashchanges. Timeout
-  // causes offsetAnchor to be called after the page jump.
-  window.setTimeout(function() {
-    offsetAnchor();
-  }, 0);
-});
-
-// Set the offset when entering page with hash present in the url
-window.setTimeout(offsetAnchor, 0);
